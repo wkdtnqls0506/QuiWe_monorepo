e739e9e42232551ea5743b3cf9e97f89ca9ea898
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-01-20 16:56:53 +0900

    Chore: 클라이언트 배포 테스트

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,4 +1,10 @@
-const HomePage = () => {
+const HomePage = async () => {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/quiz/1`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+
   return <div>home</div>
 }
 
