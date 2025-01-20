57a5a4753f62bf69c1260311cb62aa5c1684d575
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-01-20 17:07:02 +0900

    Chore: 클라이언트 배포 테스트

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,34 @@
 const HomePage = async () => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/quiz/1`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Failed to fetch data')
+      }
+      return res.json()
+    })
+    .catch((err) => {
+      console.error(err)
+      return null
     })
 
-  return <div>home</div>
+  return (
+    <div>
+      <h2>Category: {data.category}</h2>
+      <h2>Level: {data.level}</h2>
+      <h3>Details: {data.details}</h3>
+      <div>
+        <h2>Questions:</h2>
+        <ul>
+          {data.questions.map((question: any) => (
+            <li key={question.id}>
+              <strong>{question.title}</strong>
+              <p>Type: {question.type}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
