b710ff99f611b4d4b9f80ec9848d9f2e474b185f
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-01-25 23:11:17 +0900

    Feat: react query 초기 설정

---
+++ apps/client/src/providers/query-provider.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+// 서버 환경과 브라우저 환경에서 서로 다르게 생성
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+    return browserQueryClient
+  }
+}
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
