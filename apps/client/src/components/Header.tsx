1293002988bd10c2891ce1c19abcab47f669850a
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-01-29 14:57:34 +0900

    Test: 배포 테스트 및 프리티어 적용

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -1,25 +1,25 @@
-'use client'
+'use client';
 
-import logo from 'public/logo.svg?url'
-import { useRouter, usePathname } from 'next/navigation'
-import Image from 'next/image'
-import Link from 'next/link'
-import classNames from 'classnames'
+import logo from 'public/logo.svg?url';
+import { useRouter, usePathname } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import classNames from 'classnames';
 
 const Header = () => {
-  const router = useRouter()
-  const pathName = usePathname()
+  const router = useRouter();
+  const pathName = usePathname();
 
   return (
-    <header className="flex justify-between items-center px-8 py-2 border-b border-gray-300">
-      <Link href="/" className="cursor-pointer">
-        <Image src={logo} alt="로고 이미지" width={150} height={50} />
+    <header className='flex justify-between items-center px-8 py-2 border-b border-gray-300'>
+      <Link href='/' className='cursor-pointer'>
+        <Image src={logo} alt='로고 이미지' width={150} height={50} />
       </Link>
-      <div className="flex items-center">
+      <div className='flex items-center'>
         <button
           className={classNames(
             'ml-2 text-green-700 p-2 transition-all duration-200 hover:bg-green-100 rounded',
-            pathName === '/quiz' && 'bg-green-100',
+            pathName.startsWith('/quiz') && 'bg-green-100'
           )}
           onClick={() => router.push('/quiz')}
         >
@@ -28,7 +28,17 @@ const Header = () => {
         <button
           className={classNames(
             'ml-2 text-green-700 p-2 transition-all duration-200 hover:bg-green-100 rounded',
-            pathName === '/mypage' && 'bg-green-100',
+            pathName === '/portfolio' && 'bg-green-100'
+          )}
+          onClick={() => router.push('/portfolio')}
+        >
+          포트폴리오
+        </button>
+        {/*로그인 여부로 마이페이지 조건부 렌더링 필요*/}
+        <button
+          className={classNames(
+            'ml-2 text-green-700 p-2 transition-all duration-200 hover:bg-green-100 rounded',
+            pathName === '/mypage' && 'bg-green-100'
           )}
           onClick={() => router.push('/mypage')}
         >
@@ -37,7 +47,7 @@ const Header = () => {
         <button
           className={classNames(
             'ml-2 text-green-700 p-2 transition-all duration-200 hover:bg-green-100 rounded',
-            pathName === '/login' && 'bg-green-100',
+            pathName === '/login' && 'bg-green-100'
           )}
           onClick={() => router.push('/login')}
         >
@@ -45,7 +55,7 @@ const Header = () => {
         </button>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
