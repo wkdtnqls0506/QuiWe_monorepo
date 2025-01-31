3b0f8c75e097466f103e31d5d4355d1aa6b54390
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-02-01 00:14:32 +0900

    Feat: 퀴즈 생성 API 연결

--- apps/client/src/components/Quiz/Category/SubmitButton.tsx
+++ apps/client/src/components/Quiz/Category/SubmitButton.tsx
@@ -1,30 +1,44 @@
-'use client'
+'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation'
-import toast from 'react-hot-toast'
+import { createQuiz } from '@/apis/quiz';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 const SubmitButton = () => {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const detailParams = searchParams.get('detail')
-  const levelParams = searchParams.get('level')
+  const router = useRouter();
 
-  const handleClick = () => {
-    if (!detailParams || !levelParams) {
-      toast.error('세부 주제와 레벨을 반드시 선택해주세요!')
-    } else {
-      router.push('/solve')
+  const lastPath = usePathname().split('/').pop();
+
+  const searchParams = useSearchParams();
+  const detailParams = searchParams.getAll('detail');
+  const levelParams = searchParams.get('level');
+
+  const handleClick = async () => {
+    if (!detailParams || !levelParams || !lastPath) {
+      toast.error('세부 주제와 레벨을 반드시 선택해주세요!');
     }
-  }
+
+    try {
+      const quizData = await createQuiz({
+        category: lastPath || '',
+        details: detailParams,
+        level: Number(levelParams)
+      });
+
+      router.push(`/challenge/${quizData.data.id}`);
+    } catch (error) {
+      toast.error('퀴즈 생성에 실패했습니다. 다시 시도해주세요.');
+    }
+  };
 
   return (
     <button
       onClick={handleClick}
-      className="w-full mt-6 p-3 text-md rounded-lg bg-green-100 transition-all duration-300 hover:bg-green-200"
+      className='w-full mt-6 p-3 text-md rounded-lg bg-green-100 transition-all duration-300 hover:bg-green-200'
     >
       제출하고 퀴즈 풀러가기
     </button>
-  )
-}
+  );
+};
 
-export default SubmitButton
+export default SubmitButton;
