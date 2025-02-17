9e8bf4d5308cf5425e9b0f949c116cdf00d56d3f
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-02-18 00:52:15 +0900

    Chore: 퀴즈 생성 API 매개변수 이름 변경

--- apps/client/src/apis/quiz.ts
+++ apps/client/src/apis/quiz.ts
@@ -1,12 +1,12 @@
 import { TQuizRequest } from '@/types/quiz.type';
 
-export async function createQuiz(quizTypes: TQuizRequest) {
+export async function createQuiz(quizRequest: TQuizRequest) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/quiz`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(quizTypes)
+    body: JSON.stringify(quizRequest)
   });
 
   if (!res.ok) {
