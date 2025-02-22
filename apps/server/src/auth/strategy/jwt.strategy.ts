fb3a01a873a6b7ea63bca1648367a6e8571a3f73
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-02-22 22:10:36 +0900

    Feat(server): 일반 로그인 인증을 위한 전략

--- apps/server/src/auth/strategy/jwt.strategy.ts
+++ apps/server/src/auth/strategy/jwt.strategy.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AuthGuard, PassportStrategy } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export interface JwtPayload {
+  sub: number;
+  email: string;
+  name: string;
+  iat?: number;
+  exp?: number;
+}
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest<Request>();
+    const token = req.cookies['accessToken'];
+
+    if (!token) return false;
+
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+      req.user = payload;
+      return true;
+    } catch (error) {
+      console.error('JWT 검증 실패:', error.message);
+    }
+  }
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,7 +44,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('ACCESS_TOKEN_SECRET'),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
