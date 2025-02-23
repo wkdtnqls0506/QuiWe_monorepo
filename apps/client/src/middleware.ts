1a80918583a0da6fa0395c29857892d5a0818afc
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-02-23 23:07:22 +0900

    Feat(client): 미들웨어를 통해 로그인 여부에 따라 페이지 접근 제어

---
+++ apps/client/src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  let accessToken = request.cookies.get('accessToken')?.value;
+
+  if (!accessToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/quiz/:path', '/portfolio', '/challenge/:path', '/result/:path', '/mypage']
+};
