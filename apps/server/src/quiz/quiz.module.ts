9eeb5d3b6b27a8d93cbeeaf2db71123c0baf8af0
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-04-02 15:20:54 +0900

    Refactor(server): quiz 트랜잭션 적용

--- apps/server/src/quiz/quiz.module.ts
+++ apps/server/src/quiz/quiz.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuizEntity } from './entities/quiz.entity';
 import { QuizController } from './quiz.controller';
@@ -6,11 +6,13 @@ import { QuizService } from './quiz.service';
 import { QuestionModule } from 'src/question/question.module';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { ResultEntity } from 'src/result/entities/result.entity';
+import { QuizSubmissionModule } from 'src/quiz-submission/quiz-submission.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([QuizEntity, UserEntity, ResultEntity]),
     QuestionModule,
+    forwardRef(() => QuizSubmissionModule),
   ],
   controllers: [QuizController],
   providers: [QuizService],
