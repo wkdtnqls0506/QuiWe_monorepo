0a91a702f7ba29bdf7b749aaf20f2c2ce3d4a245
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-04-05 23:13:03 +0900

    Fix(server): question service 데이터 저장 방식 변경

--- apps/server/src/question/question.service.ts
+++ apps/server/src/question/question.service.ts
@@ -1,45 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
-import { QueryRunner } from 'typeorm';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { QuestionEntity } from './entities/question.entity';
 import { CreateQuizDto } from 'src/quiz/dto/create-quiz.dto';
-import { QuizEntity } from 'src/quiz/entities/quiz.entity';
 
 @Injectable()
 export class QuestionService {
   private readonly openai: OpenAI;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(QuestionEntity)
+    private readonly questionRepository: Repository<QuestionEntity>,
+  ) {
     this.openai = new OpenAI({
       apiKey: this.configService.get<string>('OPENAI_API_KEY'),
     });
   }
 
-  async create(
-    createQuizDto: CreateQuizDto,
-    queryRunner: QueryRunner,
-    quiz: QuizEntity,
-    extractedText?: string,
-  ) {
+  async create(createQuizDto: CreateQuizDto, extractedText?: string) {
     const { category, details, level } = createQuizDto;
 
-    const prompt = this.createPrompt(category, details, level, extractedText);
-    const response = await this.openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
-      max_tokens: 2000,
-      temperature: 0.7,
-    });
+    try {
+      if (!details || !level) {
+        throw new HttpException(
+          '필수 입력값이 누락되었습니다.',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
 
-    const generated = JSON.parse(response.choices[0].message?.content || '{}');
+      const prompt = this.createPrompt(category, details, level, extractedText);
 
-    const questions = generated.quizzes.map((question) => ({
-      ...question,
-      quiz,
-    }));
+      const response = await this.openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: prompt }],
+        max_tokens: 2000,
+        temperature: 0.7,
+      });
 
-    return await queryRunner.manager.save(QuestionEntity, questions);
+      const generatedQuestions = JSON.parse(
+        response.choices[0].message?.content || '{}',
+      );
+
+      return {
+        status: 'SUCCESS',
+        message: '퀴즈 문제를 성공적으로 생성했습니다.',
+        data: generatedQuestions,
+      };
+    } catch (error) {
+      console.error('퀴즈 생성 중 에러가 발생했습니다.:', error);
+      throw new HttpException(
+        '퀴즈 생성 중 에러가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async saveQuestions(questions: Partial<QuestionEntity>[]) {
+    const savedQuestions = await this.questionRepository.save(questions);
+    return savedQuestions;
   }
 
   private createPrompt(
