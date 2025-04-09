18fd7f71687f787e9e36a52e12d11e53c8dd65e5
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-04-09 17:59:32 +0900

    Feat(client): 로딩 메세지 추가

--- apps/client/src/constants/loadingMessage.ts
+++ apps/client/src/constants/loadingMessage.ts
@@ -5,3 +5,5 @@ export const CREATE_QUIZ_MESSAGES = [
 ];
 
 export const CREATE_RESULT_MESSAGES = ['사용자 답안 분석 중...', 'AI가 해설 생성 중...', '거의 완료되었습니다!🥝'];
+
+export const LOADING_MESSAGE = ['거의 완료되었습니다!🥝'];
