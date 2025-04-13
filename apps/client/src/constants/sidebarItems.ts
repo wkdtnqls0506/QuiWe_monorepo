ebc01f6190b17b5c71e638656cc2dbc5ed8bb874
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-04-14 00:06:51 +0900

    Refactor(client): 포트폴리오 탭 추가

--- apps/client/src/constants/sidebarItems.ts
+++ apps/client/src/constants/sidebarItems.ts
@@ -1,1 +1,4 @@
-export const SIDEBAR_ITEM = [{ id: 'my-quizzes', name: '내가 푼 퀴즈' }];
+export const SIDEBAR_ITEM = [
+  { id: 'my-quizzes', name: '내가 푼 퀴즈' },
+  { id: 'my-portfolios', name: '포트폴리오' }
+];
