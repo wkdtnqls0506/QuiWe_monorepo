52710c57d4407f5406e56c5978240519bc4c58f1
wkdtnqls0506 <wkdtnqls0506@naver.com>
2025-04-13 23:35:56 +0900

    Refactor(client): 사용자퀴즈 및 포트폴리오  기록이 없을 경우를 위한 공통 컴포넌트로 변경

--- apps/client/src/components/MyPage/NoHistory.tsx
+++ apps/client/src/components/MyPage/NoHistory.tsx
@@ -4,7 +4,14 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-const NoQuizHistory = () => {
+type NoHistoryProps = {
+  title: string;
+  description: string;
+  buttonText: string;
+  redirectUrl: string;
+};
+
+const NoHistory = ({ title, description, buttonText, redirectUrl }: NoHistoryProps) => {
   const router = useRouter();
 
   return (
@@ -14,17 +21,17 @@ const NoQuizHistory = () => {
       transition={{ duration: 0.5 }}
       className='flex flex-col items-center justify-center p-10 bg-white shadow-md rounded-lg text-center'
     >
-      <Image src='/images/quiweX.png' alt='No Quiz' width={40} height={40} className='mb-4' />
-      <p className='mt-4 text-xl font-semibold text-gray-700'>아직 퀴즈 기록이 없어요!</p>
-      <p className='mt-2 text-gray-500'>새로운 퀴즈를 생성하고 학습을 시작해보세요.</p>
+      <Image src='/images/quiweX.png' alt='Empty State' width={100} height={100} className='mb-4' />
+      <p className='mt-4 text-xl font-semibold text-gray-700'>{title}</p>
+      <p className='mt-2 text-gray-500'>{description}</p>
       <button
         className='mt-6 px-6 py-3 bg-green-400 text-white font-semibold rounded-lg hover:bg-green-300 transition'
-        onClick={() => router.push('/quiz')}
+        onClick={() => router.push(redirectUrl)}
       >
-        퀴즈 풀러 가기 🚀
+        {buttonText}
       </button>
     </motion.div>
   );
 };
 
-export default NoQuizHistory;
+export default NoHistory;
